3bfdb94562744d54772fb4e21d83f6daf46faf7a
zm1try <zmitryarosh@gmail.com>
2018-10-28 13:17:49 +0300

    feat: add eslint

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,5 +1,4 @@
 function sumOfOther(inputArr) {
-    return inputArr.map(item => inputArr.reduce((a,b) => a + b) - item);
+  return inputArr.map(item => inputArr.reduce((a, b) => a + b) - item);
 }
-
 module.exports = sumOfOther;
