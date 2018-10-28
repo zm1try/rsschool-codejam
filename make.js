ffee961e5d74febd227b065121c9fe30e741f426
zm1try <zmitryarosh@gmail.com>
2018-10-28 11:29:30 +0300

    feat: add make function

---
+++ make.js
@@ -0,0 +1,11 @@
+function make(input) {
+    const elems = [];
+    const foo = function step(value) {
+        if(!(value instanceof Function)) {
+            elems.push(...arguments);
+            return step;
+        }
+        else return elems.reduce(value);
+    };
+    return foo(input);
+}
